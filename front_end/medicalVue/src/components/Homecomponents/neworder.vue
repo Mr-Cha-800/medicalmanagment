b5eef1f23869e234d8291803b974866a73c1d453
mr-cha-800 <chakerdmlc@gmail.com>
2020-12-01 20:46:30 +0100

    nouvelle commande

--- front_end/medicalVue/src/components/Homecomponents/neworder.vue
+++ front_end/medicalVue/src/components/Homecomponents/neworder.vue
@@ -9,15 +9,16 @@
             <q-form
       @submit.prevent="onSubmit"
       @reset="onReset"
-      class="justify-center q-pa-sm"
+      class="justify-center q-pa-lg"
     >
+    <div class="text-h6">Informations de client</div>
       <q-input
         v-model="nom"
         type="text"
         class="q-pa-md"
         label="Votre Prénom *"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'veuillez taper votre prénom']"
+        :rules="[ val => val && val.length > 0 || 'veuillez taper le prénom']"
       >
       </q-input>
       <q-input
@@ -26,23 +27,46 @@
         class="q-pa-md"
         label="Votre Nom *"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'veuillez taper votre prénom']"
+        :rules="[ val => val && val.length > 0 || 'veuillez taper le nom']"
       >
       </q-input>
     <div class="q-gutter-sm justify-center q-pa-md">
        <q-input
         v-model="tel"
-        label="Téléphone"
-        mask="(##) ## ## ## ##"
-        fill-mask
-        hint="Aide: (##) ## ## ## ##"
-        :rules="[val => !!val || 'Obligatoire',
-          val => val && val.length > 0  || 'veuillez remplir correctement']"
+        label="Numero carte Id"
+        hint="facultatif"
       >
       </q-input>
     </div>
-      <div>
-    <q-btn no-caps type="submit" push color="blue-grey-5" :loading="loading" :disabled="loading"  icon-right="send" label="Acheter" >      <template v-slot:loading>
+    <div class="q-gutter-sm justify-center q-pa-md">
+       <q-input
+        v-model="tel"
+        label="Numero permis de conduire"
+        hint="facultatif"
+      >
+      </q-input>
+    </div>
+    <div class="text-h6">Commande</div>
+    <div class="q-gutter-sm full-width q-pa-md row">
+       <q-input
+        v-model="tel"
+        label="Produit"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'veuillez taper la reference du produit']"
+      >
+      </q-input>
+       <q-input
+        v-model="tel"
+        type="number"
+        label="Quantité"
+        min="1"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'veuillez taper la quantité']"
+      >
+      </q-input>
+    </div>
+      <div class="q-pa-md ">
+    <q-btn no-caps type="submit" push color="blue-grey-5" :loading="loading" :disabled="loading"  icon-right="send" label="Générer la facture" >      <template v-slot:loading>
         <q-spinner-hourglass class="on-left" />
         Loading...
       </template>
@@ -54,7 +78,11 @@
 
 <script>
 export default {
+  data(){
+    return {
 
+    }
+  }
 }
 </script>
 
--- front_end/medicalVue/src/views/NewOrder.vue
+++ front_end/medicalVue/src/views/NewOrder.vue
@@ -1,10 +1,15 @@
 <template>
-  <h1>new order</h1>
+  <div class="q-pa-xl q-gutter-xl">
+    <neworder/>
+  </div>
 </template>
 
 <script>
+import neworder from '../components/Homecomponents/neworder'
 export default {
-
+  components:{
+    neworder
+  }
 }
 </script>
 
