446a3ddcb55d81087970eb998f7275ad8de1ea1e
ahmed <ahmed.saadallah@etu.univ-lyon1.fr>
2020-12-01 19:50:38 +0100

    Add Invoce && Product classes

---
+++ back_end/Invoce.py
@@ -0,0 +1,8 @@
+class Invoce:
+  def __init__(self, id, fname, lname, phone_num, date, product_list):
+    self.id = id
+    self.fname = fname
+    self.lname = lname
+    self.phone_num = phone_num
+    self.date = date
+    self.product_list = product_list
