34fc93af653d97da7945fe223956ac498e727a13
mr-cha-800 <chakerdmlc@gmail.com>
2020-12-10 03:57:49 +0100

    devis et facture tres bien fait

--- back_end/routes/factures.js
+++ back_end/routes/factures.js
@@ -47,11 +47,9 @@ Router.post('/',(req,res)=>{
                     })
                 }
                 mysqlConnection.query('UPDATE devis_facture SET identifier=? WHERE identifier=?',[0,x],(err,rows,fields)=>{
-                    if(!err)
-                    res.json(rows);
-                    else
+                    if(err)
                     console.log(err);
-                })
+                });
                 res.send(rows);
             }
             else
