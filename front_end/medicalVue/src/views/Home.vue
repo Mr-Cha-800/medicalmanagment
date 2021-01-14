dcde99211d576b439ac551919fa6e7f442bb56f0
mr-cha-800 <chakerdmlc@gmail.com>
2021-01-14 17:15:22 +0100

    fix el home li ra7 yweli dashboard hh

--- front_end/medicalVue/src/views/Home.vue
+++ front_end/medicalVue/src/views/Home.vue
@@ -10,7 +10,7 @@
    </div>
    <div class="row q-pa-md justify-center full-height full-width text-center">
     <div class="col-md-5 col-sm-6 col-xs-12 q-pa-md">
-        <newdevis/>
+    <!--    <newdevis/>-->
     </div>
     <div class="col-md-5 col-sm-6 col-xs-12 q-pa-md">
     <!--    <buyinghistory/> -->
@@ -21,14 +21,14 @@
 <script>
 import savingdata from '../components/settings/savingdata'
 // import buyinghistory from '../components/Homecomponents/buyinghistory'
-import newdevis from '../components/Homecomponents/newdevis'
+// import newdevis from '../components/Homecomponents/newdevis'
 // import search from '../components/Homecomponents/search'
 export default {
     name: 'Home',
     components: { 
         savingdata,
         // buyinghistory,
-        newdevis,
+        // newdevis,
         // search
         }
 
